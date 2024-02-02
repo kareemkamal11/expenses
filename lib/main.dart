9b1cdbf0fbe97c4e15b818d53df837b8f1d00ca5
kareemkamal11 <sefi31009853@f-eng.tanta.edu.eg>
2024-02-02 11:02:07 +0200

    enf

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then((_) {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
     runApp(MyApp());
-  // });
+  });
 }
 
 class MyApp extends StatelessWidget {
